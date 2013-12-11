3adc2a32c9476d25d8f0853f87bb1347d8d4eec9
Alex Robbins <alexander.j.robbins@gmail.com>
2013-12-10 20:50:31 -0600

    Add rainbow delimiters to emacs.

--- init.el
+++ init.el
@@ -97,3 +97,6 @@
 (require 'cider)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode)
