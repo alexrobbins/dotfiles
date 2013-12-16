c5a1929f85c0c36c36e9747b62f46efeaa638ab2
Alex Robbins <alexander.j.robbins@gmail.com>
2013-12-16 10:50:35 -0600

    Drop extra clojure syntax.

--- init.el
+++ init.el
@@ -25,28 +25,6 @@
 
 (add-to-list 'default-frame-alist '(alpha 85 75))
 
-;; magic, haven't broken this down yet
-(defmacro defclojureface (name color desc &optional others)
-  `(defface ,name '((((class color)) (:foreground ,color ,@others))) ,desc :group 'faces))
-
-(defclojureface clojure-braces       "#49b2c7"   "Clojure braces")
-(defclojureface clojure-brackets     "SteelBlue" "Clojure brackets")
-(defclojureface clojure-keyword      "khaki"     "Clojure keywords")
-(defclojureface clojure-namespace    "#c476f1"   "Clojure namespace")
-(defclojureface clojure-java-call    "#4bcf68"   "Clojure Java calls")
-(defclojureface clojure-special      "#b8bb00"   "Clojure special")
-(defclojureface clojure-double-quote "#b8bb00"   "Clojure special")
-
-(defun tweak-clojure-syntax ()
-  (mapcar (lambda (x) (font-lock-add-keywords nil x))
-          '((("#?\\^?{\\|}"        . 'clojure-brackets))
-            (("\\[\\|\\]"          . 'clojure-braces))
-            ((":\\w+"              . 'clojure-keyword))
-            (("#?\""               0 'clojure-double-quote prepend))
-            (("nil\\|true\\|false\\|%[1-9]?" . 'clojure-special))
-            (("(\\(\\.[^ \n)]*\\|[^ \n)]+\\.\\|new\\)\\([ )\n]\\|$\\)" 1 'clojure-java-call)))))
-
-(add-hook 'clojure-mode-hook 'tweak-clojure-syntax)
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
 ;; Needed to define ctrl arrow keys from terminal.app
