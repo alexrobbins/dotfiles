4be85c0d457ac8695299994262109380d9a07bd1
Alex Robbins <alexander.j.robbins@gmail.com>
2017-04-27 00:30:31 +0000

    Dotfile updates

--- init.el
+++ init.el
@@ -9,26 +9,27 @@
 (setq-default indent-tabs-mode nil)
 
 ;; add all subdirs of ~/.emacs.d to your load-path
-(dolist (f (file-expand-wildcards "~/.emacs.d/*"))
-  (add-to-list 'load-path f))
+;; (dolist (f (file-expand-wildcards "~/.emacs.d/*"))
+;;   (add-to-list 'load-path f))
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(setq package-check-signature nil)
 (package-initialize)
 
+(require 'org-install)
+(require 'org)
 (setq org-startup-folded "content")
 (setq org-startup-indented "indent")
+(setq org-return-follows-link t)
+(setq org-hide-leading-stars t)
+(define-key global-map "\C-cl" 'org-store-link)
 
-;; load color-theme
-(require 'color-theme)
 (color-theme-initialize)
-(setq color-theme-is-global t)
-(color-theme-euphoria)
+(color-theme-solarized)
 
 (add-to-list 'default-frame-alist '(alpha 85 75))
-
-(add-hook 'clojure-mode-hook 'paredit-mode)
 
 ;; Needed to define ctrl arrow keys from terminal.app
 (define-key input-decode-map "\e[5C" [C-right])
@@ -45,14 +46,14 @@
 (put 'scroll-left 'disabled nil)
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:height 140 :family "Monaco")))))
 
 ;; enable awesome file prompting
-(when (> emacs-major-version 21)
+(when (> emacs-major-version 20)
   (ido-mode t)
   (setq ido-enable-prefix nil
         ido-enable-flex-matching t
@@ -77,9 +78,36 @@
 (define-key evil-normal-state-map (kbd "TAB") nil) ; So tab works in org mode
 (define-key evil-normal-state-map (kbd "M-.") nil)
 
-(require 'cider)
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(require 'smartparens-config)
+(global-set-key (kbd "C-<right>") 'sp-forward-slurp-sexp)
+(global-set-key (kbd "C-<left>") 'sp-forward-barf-sexp)
+(setq sp-highlight-wrap-overlay nil)
+(setq sp-highlight-pair-overlay nil)
+(setq sp-highlight-wrap-tag-overlay nil)
 
-(require 'rainbow-delimiters)
-(global-rainbow-delimiters-mode)
+ 
+(add-hook 'prog-mode-hook #'which-key-mode)
+(add-hook 'prog-mode-hook #'smartparens-strict-mode)
+
+(add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
+(add-hook 'cider-mode-hook #'eldoc-mode)
+(setq cider-repl-display-help-banner nil)
+
+(global-company-mode)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'aggressive-indent-mode)
+
+(add-hook 'java-mode-hook (lambda ()
+                            (setq c-basic-offset 2
+                                  tab-width 2
+                                  indent-tabs-mode nil)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(frame-background-mode (quote dark)))
